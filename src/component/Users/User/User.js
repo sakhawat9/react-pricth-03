9a7aff0b5bc10ebcfb26429059bed00813a5eb62
SakhawatHossain55 <76767141+SakhawatHossain55@users.noreply.github.com>
2021-03-03 02:55:09 +0600

    added style

--- src/component/Users/User/User.js
+++ src/component/Users/User/User.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faCheck } from '@fortawesome/free-solid-svg-icons'
 import './User.css'
 
 const User = (props) => {
-    console.log(props)
     const { name, email, phone, picture } = props.user;
+    const addMember = props.addMember;
+    const fullName = name.title + '.' + name.first + ' ' + name.last
 
+    const [displayMobile, setDisplayMobile] = useState('')
+    const [isClicked, setIsClicked] = useState(false)
+
+    const showPhoneNumber = () => setDisplayMobile(phone)
+    // const 
     return (
         <div style={{width: '500px'}} className="d-flex border m-3 rounded-3">
             <div className="p-2 rounded">
                 <img className="img-thumbnail rounded" src={picture.large}/>
             </div>
             <div className="p-2">
-                <h5>Name : {name.title + '.' + name.first + ' ' + name.last}</h5>
+                <h5>Name : {fullName}</h5>
                 <p>Email : {email}</p>
                 <p>Learn about me</p>
-                <p>Phone : </p>
-                <button className="btn btn-info">ADD</button>
+                <p>Phone : {displayMobile}</p>
+                
+                <button onClick={showPhoneNumber} className="btn btn-info me-2">Sho Number</button>
+                {/* <button onClick={() => addMember(fullName)} className="btn btn-info">ADD ME</button> */}
+                <button disabled={isClicked} class=" btn btn-info" onClick={() => {
+                        addMember(fullName)
+                        setIsClicked(true)
+                        }}>
+                        {isClicked ? <FontAwesomeIcon icon={faCheck} /> : <FontAwesomeIcon icon={faPlus} />}
+                    </button>
+                
             </div>
         </div>
     );
--- src/component/Users/Users.js
+++ src/component/Users/Users.js
@@ -3,18 +3,31 @@ import User from './User/User';
 
 const Users = () => {
     const [userData, setUserData] = useState([])
+    const [team, setTeam] = useState([])
     useEffect(() => {
         fetch('https://randomuser.me/api/?results=15')
-        .then(res => res.json())
-        .then(data => setUserData(data.results))
-        
+            .then(res => res.json())
+            .then(data => setUserData(data.results))
     }, [])
+
+    const addMember = (name) => {
+        setTeam([...team, name])
+    }
     return (
-        <div>
+        <div className="container">
             <h3>Team Build</h3>
-            {
-                userData.map(user => <User user={user}></User>)
-            }
+            <div style={{width: '45%'}} className="border rounded ">
+                <ul className="list-group">
+                    {
+                        team.map(man => <li className="list-group-item">{man}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="row">
+                {
+                    userData.map(user => <User addMember={addMember} user={user}></User>)
+                }
+            </div>
         </div>
     );
 };
